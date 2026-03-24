f7ed1c18ba76096d6acf848fb03fda36eb16ca51
Anyine Elizabeth <anyineelizabeth9@gmail.com>
2026-03-24 12:27:07 +0300

    added fields to weekly logs

--- backend/evaluations/models.py
+++ backend/evaluations/models.py
@@ -1,23 +1,24 @@
 from django.db import models
 from django.conf import settings
+from django.core.exceptions import ValidationError
 
 
 class Evaluation(models.Model):
     student = models.ForeignKey(settings.AUTH_USER_MODEL,
-    on_delete=models.CASCADE, related_name='evaluations_as_student',
+    on_delete=models.PROTECT, related_name='evaluations_as_student',
     limit_choices_to = {'role':'student'})
 
-    supervisor = models.ForeignKey(settings.AUTH_USER_MODEL, 
+    evaluator = models.ForeignKey(settings.AUTH_USER_MODEL, 
     on_delete = models.CASCADE, related_name= 'evaluations_as_supervisor',
-    limit_choices_to = {'role': 'supervisor'})
-
+    limit_choices_to = {'role': 'workplace_supervisor'})
     evaluation_type = models.CharField(max_length=10)
     status = models.CharField(max_length=15)
-
     overall_remarks = models.TextField(blank=True)
     submitted_at = models.DateTimeField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    
 
 
 class EvaluationCriteria(models.Model):
@@ -27,6 +28,19 @@ class EvaluationCriteria(models.Model):
     category = models.CharField(max_length=30)
     evaluator_role = models.CharField(max_length=30)
     is_active = models.BooleanField(default=True)
-
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class EvaluationScore(models.Model):
+    evaluation = models.ForeignKey(Evaluation, on_delete=models.CASCADE, related_name='scores')
+    criteria = models.ForeignKey(EvaluationCriteria,
+    on_delete=models.PROTECT,related_name='scores')
+
+    score_awarded = models.DecimalField(max_digits=5, decimal_places=2)
+    comment = models.TextField(blank=True)
+
+class Meta:
+    unique_together = ('evaluation','criteria')
+def _str_(self):
+    return f"{self.criteria.title}:{self.score_awarded}/{self.critera.max_score}"
+
--- backend/logs/models.py
+++ backend/logs/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 from django.conf import settings
+from placements.models import InternshipPlacement
 
 
 
 class WeeklyLogs(models.Model):
-    student = models.ForeignKey(settings.AUTH_USER_MODEL, 
-    on_delete = models.CASCADE, related_name = 'weekly_logs') 
+    STATUS = (
+        ("draft", "Draft"),
+        ("submitted", "Submitted"),
+        ("under_review", "Under Review"),
+        ("endorsed","Endorsed"),
+        ("resubmit", "Resubnit"),
+        ("assessed", "Assessed"),
+        ("closed", "Closed")
+    ) 
+    
+    placement = models.ForeignKey(InternshipPlacement,
+     models.CASCADE,related_name='weekly_logs')
     week_number = models.PositiveSmallIntegerField()  # 1–12
     week_start_date = models.DateField()
     week_end_date = models.DateField()
@@ -17,7 +28,26 @@ class WeeklyLogs(models.Model):
         help_text="Difficulties encountered and how resolved"
     )
     student_remarks = models.TextField(blank=True, help_text="Any additional remarks")
-    status = models.CharField(max_length=20)
+    status = models.CharField(max_length=20, choices= STATUS)
+    workplace_comment = models.TextField(blank=True, null=True)
+    workplace_endorsed_by = models.ForeignKey(settings.AUTH_USER_MODEL,
+    on_delete=models.SET_NULL, null=True, blank=True, related_name="endorsed_logs", limit_choices_to={"role":"workplace_supervisor"})
+    workplace_endorsed_at = models.DateTimeField(blank=True, null=True) 
+    academic_comment = models.TextField(blank=True, null=True)
+    academic_grade = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    academic_assessed_by = models.ForeignKey(settings.AUTH_USER_MODEL, 
+    on_delete = models.SET_NULL, related_name="assessed_logs", limit_choices_to={"role":"academic_supervisor"})
+    academic_assessed_at = models.DateTimeField(blank=True, null=True)
     submitted_at = models.DateTimeField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Weekly Log"
+        verbose_name_plural = "Weekly Logs"
+        ordering = ["placement", "week_number"]
+        unique_together = ("placement", "week_number")
+    
+    def __str__(self):
+        return f"Week {self.week_number}--{self.placement.student.get_full_name()}"
+
