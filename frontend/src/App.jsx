de19c039760cac4438c1d7799df8b1f86ccb2cd5
mariam968-code <bisirikirwamariam968@gmail.com>
2026-04-08 14:03:59 +0300

    fixed some code in main.jsx by changing on line 2 react-router to react-dom

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+JSX 
+
+function App() {
+  return <h1>APP IS WORKING</h1>;
+
+}
+
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from 'react-router'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {BrowserRouter} from 'react-dom';
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -10,4 +10,4 @@ createRoot(document.getElementById('root')).render(
       <App />
     </BrowserRouter>
   </StrictMode>,
-)
+);
