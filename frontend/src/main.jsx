b730a7963419ca098dfd4553584e101c6a2808a6
Joan-bit-commits <jnakamyuka12@gmail.com>
2026-04-10 21:13:25 +0300

    Modified main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import './index.css'
 
 
-ReactDOM.createRoot(document.getElementByld("root")).render(
-  <App/>
+createRoot(document.getElementByld("root")).render(
+  <StrictMode>
+  <App/>  
+  </StrictMode>
+
 );
