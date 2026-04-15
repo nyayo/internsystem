1e3bbeadb6e4bd84191b81c21553ced55860264c
Joan-bit-commits <jnakamyuka12@gmail.com>
2026-04-15 12:34:51 +0300

    Changed LoginPage.jsx

--- frontend/src/pages/login/LoginPage.jsx
+++ frontend/src/pages/login/LoginPage.jsx
@@ -7,45 +7,51 @@ function LoginPage() {
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
 
-    const handleLogin = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
         if (!email || !password) {
             setError("Please fill in all the fields.");
             return;
 
         };
         setError("");
-        console.log("Logging in:", {email, password});
+        console.log("Logging in with:", {email, password});
+
     
 }
     return (
     <div className="login-container">
-        <h1 className="login-title">Welcome</h1>
-        <p className="login-substitle">Sign in to your account</p>
+        <form className="login-card"
+        onSubmit={handleSubmit}>
 
-        {error && <p className="login-error">{error}</p>}
+        <h2>Internship System</h2>
+        <p>Login to continue</p>
+
+        {error && <p className="error message">{error}</p>}
 
         <input
-            className="login-input"
+            
             type="email"
-            placeholder="Email"
+            placeholder="Enter your email"
             value={email}
             onChange={(e) =>setEmail(e.target.value)}
         />
         <input
-            className="login-input"
+            
             type="password"
-            placeholder="Password"
+            placeholder="Enter your password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
         />
 
-        <button className="login-btn" onClick={handleLogin}>
+        <button type="submit" >
             Login
         </button>
 
-        <p className="login-footer">
-            Don't have an account? <a href="/register">Sign up</a>
-        </p>
+        <div classNme="login-links">
+            <a href="#">Forgot Password?</a>
+        </div>
+        </form>
 
     </div>
     );
