a20994664bbc0b8422c1f67c911525f1db579e62
mariam968-code <bisirikirwamariam968@gmail.com>
2026-04-16 17:05:21 +0300

    feat:add sidebar.css for adminDashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,16 @@
-import AdminDashboard from "./pages/AdminDashboard";
+import RegisterPage from "./pages/auth/register/RegisterPage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import './App.css';
+
+
+function App() {
+  return (
+    <RegisterPage />
+    // <LoginPage />
+  );
+
+}
+
+export default App;
 
 
