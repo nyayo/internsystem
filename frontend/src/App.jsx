aff80dbcba8b425c5eece6e4764718e3ce7d3127
Ivan Nyayo <ivannyayo@gmail.com>
2026-04-17 00:02:16 +0300

    Moved Sidebar component to admin folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import RegisterPage from "./pages/auth/register/RegisterPage";
 import LoginPage from "./pages/auth/login/LoginPage";
+import AdminDashboard from "./pages/admin/AdminDashboard";
 import './App.css';
 
 
 function App() {
   return (
-    <RegisterPage />
+    <AdminDashboard />
     // <LoginPage />
   );
 
---
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -0,0 +1,17 @@
+import Sidebar from "../../components/admin/Sidebar"
+
+
+
+function AdminDashboard(){
+  return(
+    <div className="container">
+      <Sidebar />
+    <div>
+      <h1>Admin Dashboard ✅</h1>
+      <p>Your page is now working</p>
+      </div>
+    </div>
+  );
+}
+
+export default AdminDashboard
