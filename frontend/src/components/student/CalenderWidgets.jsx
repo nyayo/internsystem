95be016596dad6fe37d1d86224e06e52c2eaadc4
Joan-bit-commits <jnakamyuka12@gmail.com>
2026-04-23 00:32:41 +0300

    Added CalenderWidget.jsx components

--- frontend/src/components/student/CalenderWidgets.jsx
+++ frontend/src/components/student/CalenderWidgets.jsx
@@ -1,2 +1,4 @@
 import React, { useState } from 'react';
 import './CalendarWidget.css';
+export default function CalendarWidget({ startDate, endDate }) {
+  const [currentMonth, setCurrentMonth] = useState(new Date());
