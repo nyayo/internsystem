1011ca7f20747c62e982e4cfd91c1756781a3803
Joan-bit-commits <jnakamyuka12@gmail.com>
2026-04-23 00:38:20 +0300

    Added component to compute current data of the month

--- frontend/src/components/student/CalenderWidgets.jsx
+++ frontend/src/components/student/CalenderWidgets.jsx
@@ -7,4 +7,12 @@ export default function CalendarWidget({ startDate, endDate }) {
   const internshipStart = new Date(startDate);
   const internshipEnd = new Date(endDate);
   const today = new Date();
+
+  const year = currentMonth.getFullYear();
+  const month = currentMonth.getMonth();
+  const firstDay = new Date(year, month, 1);
+  const lastDay = new Date(year, month + 1, 0);
+  const startingDayOfWeek = firstDay.getDay();
+  const daysInMonth = lastDay.getDate();
+
   
