61f8ef029ac657f744033c35f18ec659816f7915
Joan-bit-commits <jnakamyuka12@gmail.com>
2026-04-22 21:52:34 +0300

    Defined the component structure

---
+++ frontend/src/components/student/StudentSideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { studentNavLinks } from '../../data/studentDashboardData';
+import './StudentSideBar.css';
+
+export default function StudentSideBar({ activeLink, onNavClick, isOpen, onToggle, pendingCount }) {
+  return (
+    <aside>
+        
+    </aside>
+  );
+}
