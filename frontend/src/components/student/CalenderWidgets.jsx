456474d65e44587222ab23bf4a091ea2b0fa794f
Joan-bit-commits <jnakamyuka12@gmail.com>
2026-04-23 00:35:36 +0300

    added the data variables in calenderWidgets

--- frontend/src/components/student/CalenderWidgets.jsx
+++ frontend/src/components/student/CalenderWidgets.jsx
@@ -2,3 +2,9 @@ import React, { useState } from 'react';
 import './CalendarWidget.css';
 export default function CalendarWidget({ startDate, endDate }) {
   const [currentMonth, setCurrentMonth] = useState(new Date());
+
+   
+  const internshipStart = new Date(startDate);
+  const internshipEnd = new Date(endDate);
+  const today = new Date();
+  
