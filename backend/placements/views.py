c0dce36d57d20c8edda407d43d8616cd98f13479
Anyine Elizabeth <anyineelizabeth9@gmail.com>
2026-04-28 10:23:51 +0300

    added extend_schema to views.py in placements

--- backend/placements/views.py
+++ backend/placements/views.py
@@ -4,6 +4,9 @@ from rest_framework          import status
 from rest_framework.views    import APIView
 from rest_framework.response import Response
 from rest_framework.parsers  import MultiPartParser, FormParser, JSONParser
+from rest_framework.permissions import IsAuthenticated
+from drf_spectacular.utils import extend_schema, OpenApiResponse, inline_serializer, OpenApiParameter
+from rest_framework import serializers as drf_serializers
 
 from .models import InternshipPlacement
 from .serializers import (
