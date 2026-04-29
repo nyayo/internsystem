f46a7c2a47efd1bdb97f6219bbfcd3968b9e559a
Joan-bit-commits <jnakamyuka12@gmail.com>
2026-04-29 18:59:03 +0300

    Created base academic dashboard component

---
+++ frontend/src/pages/supervisor-academic/AcademicDashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AcademicDashboard() {
+  return (
+    <div className="dashboard">
+      <h2>Academic Supervisor Dashboard</h2>
+    </div>
+  );
+}
