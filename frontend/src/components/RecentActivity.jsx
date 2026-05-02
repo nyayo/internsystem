1f8f0414a38e71a505150877dcdfc54e81239ab7
Joan-bit-commits <jnakamyuka12@gmail.com>
2026-05-02 20:19:44 +0300

    Added render to activity list items

--- frontend/src/components/RecentActivity.jsx
+++ frontend/src/components/RecentActivity.jsx
@@ -2,7 +2,13 @@ export default function RecentActivity() {
   return(
      <div>
         <h3>Recent Activity</h3>
-      <ul></ul>
+      <ul>
+        {activity.map((item) => (
+    <li key={item.id}>
+      <strong>{item.title}</strong> - {item.studentName}
+    </li>
+  ))}
+      </ul>
      </div>
   );
 
