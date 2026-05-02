469b496c108c5d16adf5cf46a0c6632e36d36365
Joan-bit-commits <jnakamyuka12@gmail.com>
2026-05-02 19:54:00 +0300

    Added table structure for students

--- frontend/src/components/AcademicStudentsTable.jsx
+++ frontend/src/components/AcademicStudentsTable.jsx
@@ -1,3 +1,21 @@
 export default function AcademicStudentsTable({ students }) {
-  return <div></div>;
+  return (
+  <div>
+    <h3>Assigned Students</h3>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Programme</th>
+            <th>Year</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+
+        <tbody></tbody>
+      </table>
+    </div>
+  );
+  
 }
