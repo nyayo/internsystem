dbd7fd7b252a61a6f844fd8f4a3f78bf613db434
Joan-bit-commits <jnakamyuka12@gmail.com>
2026-05-03 17:35:37 +0300

    Created academic main panel component with basic layout

---
+++ frontend/src/supervisor/Academic/AcademicMainPanel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AcademicMainPanel = () => {
+  return (
+    <main>
+      <h1>Academic Supervisor</h1>
+    </main>
+  );
+};
+
+export default AcademicMainPanel;
