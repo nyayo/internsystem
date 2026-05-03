7c394dd1268b5d8786395abb22a91abcc5b3d6f1
Joan-bit-commits <jnakamyuka12@gmail.com>
2026-05-03 17:55:02 +0300

    Implemented top bar with menu button and theme toggler

--- frontend/src/supervisor/Academic/AcademicRightPanel.jsx
+++ frontend/src/supervisor/Academic/AcademicRightPanel.jsx
@@ -7,4 +7,12 @@ const AcademicRightPanel = ({ onMenuClick }) => {
   const { supervisor, stats } = useSupervisor();
 
   return (
-    <div className="right"></div>
+    <div className="right">
+          <div className="top">
+        <button onClick={onMenuClick}>
+          <span className="material-icons-sharp">menu</span>
+        </button>
+        <div className="theme-toggler" onClick={toggleTheme}>
+          <span className={`material-icons-sharp ${!isDarkMode ? 'active' : ''}`}>light_mode</span>
+          <span className={`material-icons-sharp ${isDarkMode ? 'active' : ''}`}>dark_mode</span>
+        </div>
