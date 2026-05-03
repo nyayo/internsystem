9cfb5997cac125baf1de56009a122c15151af9b2
Joan-bit-commits <jnakamyuka12@gmail.com>
2026-05-03 18:03:13 +0300

    Created academic side bar component structure

---
+++ frontend/src/supervisor/Academic/AcademicSideBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AcademicSideBar = ({ activeLink, onLinkClick, pendingLogs, pendingEvals, onClose, isOpen }) => {
+  return (
+    <aside>
+    </aside>
+  );
+};
+
+export default AcademicSideBar;
